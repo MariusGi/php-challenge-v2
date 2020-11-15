5f325256bcfe95771e15c93f35d25244418736ac
MariusGi <mariusgirnis@gmail.com>
2020-11-15 22:46:23 +0200

    add simple ui and save messages to db

---
+++ migrations/Version20201115202736.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201115202736 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE chat_messages (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, request_message LONGTEXT NOT NULL, response_message LONGTEXT NOT NULL, send_datetime DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE chat_messages');
+    }
+}
--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,19 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\ChatMessages;
+use App\Form\ChatMessageType;
+use App\Jurgis;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChatController extends AbstractController
 {
     /**
-     * @Route("/", name="chat")
+     * @Route("/", name="chat", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $chatMessages = new ChatMessages();
+        $form = $this->createForm(ChatMessageType::class, $chatMessages);
+        $responseMessage = '';
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $jurgis = new Jurgis();
+
+            $requestMessage = $chatMessages->getRequestMessage();
+            $currentDateTime = new DateTime();
+            $responseMessage = $jurgis->responds($requestMessage);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $chatMessages->setResponseMessage($responseMessage);
+            $chatMessages->setSendDatetime($currentDateTime);
+
+            $entityManager->persist($chatMessages);
+            $entityManager->flush();
+        }
+
         return $this->render('chat/index.html.twig', [
-            'controller_name' => 'ChatController',
+            'form' => $form->createView(),
+            'responseMessage' => $responseMessage,
         ]);
     }
 }
